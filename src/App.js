7577b92d8fcd9955204b495c8e738520f5447765
stevenlillis <slillis01@qub.ac.uk>
2021-01-27 00:13:08 +0000

    Finshed Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Folder name='Desktop'>
+        <Folder name='Music'>
+          <File name='all_star.mp4' />
+          <File name='express_file.mp4' />
+        </Folder>
+        <File name='cat.jpeg' />
+        <File name='dog.png' />
+      </Folder>
+      <Folder name='Application' />
     </div>
   );
+}
+
+const Folder = ({ name, children }) => {
+
+  const [isOpen, setIsOpen] = useState(true)
+
+  const direction = isOpen ? 'down' : 'right';
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  }
+
+  const indent = { marginLeft: '17px' }
+  return (
+    <div>
+      <span onClick={handleClick}>
+        <i className="red folder icon"></i>
+        <i className={`caret ${direction} icon`}></i>
+      </span>{name}
+      <div style={indent}>
+        {isOpen ? children : null}
+      </div>
+    </div>
+  )
+}
+
+const File = ({ name }) => {
+
+  const fileExtension = name.split('.')[1];
+  const fileIcons = { 
+    mp4: 'headphones', 
+    jpeg: 'file image',
+    png: 'file image outline'
+  };
+  
+  return (
+    <div>
+      <i className={`caret ${fileIcons[fileExtension]} icon`}></i>{name}
+    </div>
+  )
 }
 
 export default App;
